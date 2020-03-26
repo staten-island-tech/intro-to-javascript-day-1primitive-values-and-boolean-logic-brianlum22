47312b7e78385d5a223c260669c7a9d327bb5abf
brianlum22 <57498774+brianlum22@users.noreply.github.com>
2020-03-26 14:32:41 -0400

    javascript might be better (?)

---
+++ .vscode/index.js
@@ -0,0 +1,73 @@
+/*let string = "string";
+let sentence = "Hello " + "Brian";
+let sentence2 = " and Kenny";
+/*let combine = sentence + sentence2;
+let combine = `${sentence}${sentence2}`;
+
+let student1 = "Amy";
+let student2 = "Becky";
+let courseName = "AP COMP SCI";
+let teacherName = "Whalen";
+let longSentence = `${student1} and ${student2} will be taking 
+${courseName} with ${teacherName} next year.`;
+console.log(longSentence);
+
+let year = 2020;
+let nextYear = 2020 + 1;
+let past = 2020 / 2;
+let farFuture = 2020 * 50;
+
+let trueStatement = true;
+let falseStatement = false;
+
+let Ron;
+console.log(Ron);
+
+let student = "Caroline";
+let age = 15;
+
+console.log(student === "Caroline")
+console.log(student === "caroline")
+
+console.log("Brian");
+
+let school = "Elementary";
+if (school === "Elementary"){
+    console.log("You are in k-5");
+}   else if (school === "Middle") {
+    console.log ("You are in 6, 7, or 8th grade");
+}   else {
+    console.log("You are in HS");
+}*/
+
+let light = "yellow";
+if (light === "green") {
+    console.log("go");
+} else if (light === "yellow") {
+    console.log("caution");
+} else {
+    console.log("stop");
+}
+
+let kristyLactInt = "true";
+let baoBaoLactInt = "true";
+
+if (kristyLactInt === true &&  baoBaoLactInt == true) {
+    console.log("Get Almond Milk");
+} else if (kristyLactInt === true || baoBaoLactInt == true) {
+    console.log ("Get Both");
+} else {
+    console.log("Only milk");
+}
+
+let temp = 72;
+if (temp < 68) {
+    console.log("too cold");
+} else if (temp >= 68 && temp <= 72) {
+    console.log("perfect");
+} else if (temp > 72 && temp <= 76 ) {
+    console.log("warm");
+} else {
+    console.log ("too hot");
+}
+
